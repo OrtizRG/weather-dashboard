de23ad128094484bb17fee9bbc7927b425bc5031
Reyna <reyna.ortiz84@gmail.com>
2021-10-22 16:18:52 -0700

    adding api fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,45 @@
-var currentDatetime = document.querySelector("#datetime-current");
+var getCityData = function() {
+    fetch("https://api.github.com/users/octocat/repos");
+  };
+  
+  getCityData();
 
-var currentTime = moment();
+// const currentDatetime = document.querySelector("#datetime-current");
 
-currentDatetime.textContent = "Atlanta " + currentTime.format("MMM DD, YYYY");
+// const currentTime = moment();
 
-//
+// currentDatetime.textContent = "Atlanta " + currentTime.format("MMM DD, YYYY");
 
+// //
 
-const inputCity = document.getElementById("inputCity");
-const buttonSearch = document.getElementById("buttonSearch");
+// const searchCity = document.getElementById('searchCity');
+// const inputCity = document.getElementById("inputCity");
 
-//Get Value of Search Bar
-function formSubmit(event) {
-    console.log(event.target);
-    let city = inputCity.value.split(' ').join('_');
+// //Get Value of Search Bar
+// function formSubmit(event) {
+//     console.log(event.target);
+//     const city = inputCity.value.split(' ').join('_');
+//     console.log(city);
 
-    let weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={1cd225897e08c11da783f6cc763124b0}" + city;
-
-    console.log(weatherApi);
-   
-}
-
-//formSubmit(e)
-console.log(searchCity)
-
-searchCity.addEventListener("click", function(event) {
-    formSubmit(event);
+//     const weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=37.39&lon=-122.08&exclude=hourly,minutely&appid=1cd225897e08c11da783f6cc763124b0";
     
-});
+//     console.log(weatherApi);
+
+//     fetch(weatherApi)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     })
+
+//     //fetch("https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=AIzaSyDpiNPP_a5r4hUblmDU8cnCJnlVlDqhCgA")
+   
+// }
+
+// searchCity.addEventListener("click", function(event) {
+//     formSubmit(event);
+    
+// });
 
 
-function add(x, y) {
-
-}
-
-add(2, 3);
