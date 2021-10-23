8ff98166db9ee7b5c486078d4b48b73bbfe9487d
Reyna <reyna.ortiz84@gmail.com>
2021-10-22 17:42:39 -0700

    "search button functionality"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,51 @@
-var getCityData = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+// var getCityData = function() {
+//     fetch("http://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=1cd225897e08c11da783f6cc763124b0");
+// };
+//     var response = fetch("http://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=1cd225897e08c11da783f6cc763124b0")
+//     .then(function(response)
+//     console.log(response)
+// });
+//     console.log("inside", response);
   
-  getCityData();
+  
+//   getCityData();
 
-// const currentDatetime = document.querySelector("#datetime-current");
 
-// const currentTime = moment();
+const currentDatetime = document.querySelector("#datetime-current");
 
-// currentDatetime.textContent = "Atlanta " + currentTime.format("MMM DD, YYYY");
+const currentTime = moment();
 
-// //
 
-// const searchCity = document.getElementById('searchCity');
-// const inputCity = document.getElementById("inputCity");
+const searchCity = document.getElementById("searchCity");
+const inputCity = document.getElementById("inputCity");
 
 // //Get Value of Search Bar
-// function formSubmit(event) {
-//     console.log(event.target);
-//     const city = inputCity.value.split(' ').join('_');
-//     console.log(city);
+function formSubmit(event) {
+    console.log(event.target);
+    const city = inputCity.value.split(' ').join('_');
+    console.log(city);
 
-//     const weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=37.39&lon=-122.08&exclude=hourly,minutely&appid=1cd225897e08c11da783f6cc763124b0";
+    const weatherApi = "http://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=1cd225897e08c11da783f6cc763124b0";
     
-//     console.log(weatherApi);
+    console.log(weatherApi);
 
-//     fetch(weatherApi)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-//     })
+    fetch(weatherApi)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
 
-//     //fetch("https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=AIzaSyDpiNPP_a5r4hUblmDU8cnCJnlVlDqhCgA")
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=1cd225897e08c11da783f6cc763124b0")
    
-// }
+};
 
-// searchCity.addEventListener("click", function(event) {
-//     formSubmit(event);
+searchCity.addEventListener("click", function(event) {
+    formSubmit(event);
     
-// });
+});
+
+currentDatetime.textContent = "Sacramento " + currentTime.format("MMM DD, YYYY");
 
 
